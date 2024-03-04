5e7096fca6fd160a0f16033df4ffa84d3b36c949
Dipak <59242416+dpk6691@users.noreply.github.com>
2024-03-04 15:14:20 +0530

    Hero Banner updated

--- src/App.jsx
+++ src/App.jsx
@@ -16,21 +16,27 @@ import PageNotFound from "./pages/404/PageNotFound";
 function App() {
   const dispatch = useDispatch();
   const { url } = useSelector((state) => state.home);
-  console.log(url);
+  // console.log(url);
 
   useEffect(() => {
-    apiTest();
+    fetchApiConfig();
   }, []);
 
-  const apiTest = () => {
-    fetchDataFromApi("/movie/latest").then((res) => {
-      dispatch(getApiConfigration(res));
+  const fetchApiConfig = () => {
+    fetchDataFromApi("/configuration").then((res) => {
+      const url = {
+        backdrop: res.images.secure_base_url + "original",
+        poster: res.images.secure_base_url + "original",
+        profile: res.images.secure_base_url + "original",
+      };
+      dispatch(getApiConfigration(url));
     });
   };
 
   return (
     <>
       <BrowserRouter>
+        <Header />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/:mediaType/:id" element={<Details />} />
@@ -38,6 +44,7 @@ function App() {
           <Route path="/explore/:mediaType" element={<Explore />} />
           <Route path="*" element={<PageNotFound />} />
         </Routes>
+        <Footer />
       </BrowserRouter>
     </>
   );
--- src/pages/home/heroBanner/HeroBanner.jsx
+++ src/pages/home/heroBanner/HeroBanner.jsx
@@ -1,8 +1,65 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+
 import "./style.scss";
 
+import useFetch from "../../../hooks/useFatch";
+
+import Img from "./../../../components/lazyLoading/Img";
+
+import ContentWrapper from "./../../../components/contentWrapper/ContentWrapper";
+
 const HeroBanner = () => {
-  return <div>Banner</div>;
+  const [background, setBackground] = useState("");
+  const [query, setQuery] = useState("");
+  const navigate = useNavigate();
+  const { url } = useSelector((state) => state.home);
+
+  const { data, loading } = useFetch("/movie/upcoming");
+
+  useEffect(() => {
+    const bg =
+      url.backdrop +
+      data?.results?.[Math.floor(Math.random() * 20)]?.backdrop_path;
+    setBackground(bg);
+  }, [data]);
+
+  const searchQueryHandler = (e) => {
+    if (e.key === "Enter" && query.length > 0) {
+      navigate(`/search/${query}`);
+    }
+  };
+  return (
+    <>
+      <div className="heroBanner">
+        {!loading && (
+          <div className="backdrop-img">
+            <Img src={background} />
+          </div>
+        )}
+
+        <div className="opacity-layer"></div>
+        <ContentWrapper>
+          <div className="heroBannerContent">
+            <span className="title">Welcome</span>
+            <span className="subTitle">
+              Millions of movies, TV shows and people to discover. Explore now.
+            </span>
+            <div className="searchInput">
+              <input
+                type="text"
+                onChange={(e) => setQuery(e.target.value)}
+                onKeyUp={searchQueryHandler}
+                placeholder="Search for a movie or TV show..."
+              />
+              <button>Search</button>
+            </div>
+          </div>
+        </ContentWrapper>
+      </div>
+    </>
+  );
 };
 
 export default HeroBanner;
