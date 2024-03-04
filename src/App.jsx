e6aea799aca611d398e1f30599d5e80316dc77a9
Dipak <59242416+dpk6691@users.noreply.github.com>
2024-03-04 13:25:19 +0530

    redux added

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,29 @@
 import { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import { fetchDataFromApi } from "./utils/api";
+import { getApiConfigration } from "./store/homeSlice";
 
 function App() {
+  const dispatch = useDispatch();
+  const { url } = useSelector((state) => state.home);
+  console.log(url);
+
   useEffect(() => {
     apiTest();
   }, []);
 
   const apiTest = () => {
     fetchDataFromApi("/movie/latest").then((res) => {
-      console.log(res);
+      dispatch(getApiConfigration(res));
     });
   };
 
   return (
     <>
-      <div>Dipak</div>
+      <div>
+        Dipak
+        {url?.id}
+      </div>
     </>
   );
 }
